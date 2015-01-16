ace1c6aaf2de928651225d59881795a40de57261
Meg Pirrung <meganap@gmail.com>
2015-01-15 21:48:56 -0700

    fixing indentation issues

--- js/app.js
+++ js/app.js
@@ -40,94 +40,94 @@ app.directive('id3ogram', ['$http', '$window', function($http,$window) {
 
             // init method to set up the data structures, and vis variables
             $scope.init = function() {
-                  // link the select box to the chromosome numbers available
-                  $scope.chromosomes = Object.keys(dataByCh);
-                  // link the header to the current chromosome view
-                  $scope.currentChromosome = $scope.chromosomes[0]
+                // link the select box to the chromosome numbers available
+                $scope.chromosomes = Object.keys(dataByCh);
+                // link the header to the current chromosome view
+                $scope.currentChromosome = $scope.chromosomes[0]
 
-                  rainbow.setSpectrum('#dddddd','#000000');
+                rainbow.setSpectrum('#dddddd','#000000');
 
-                  // tooltip that holds the band label when it is visible
-              	  div = d3.select("#vis").append("div")
+                // tooltip that holds the band label when it is visible
+              	div = d3.select("#vis").append("div")
               		.attr("class", "tooltip")
               		.style("opacity", 0)
                     .style("color","#3ca6dc");
 
-                  svg = d3.select("#vis").append("svg")
-                     .attr("height", 60);
+                svg = d3.select("#vis").append("svg")
+                    .attr("height", 60);
 
-                  $scope.resetCurrentVis();
-              }
+                $scope.resetCurrentVis();
+            }
 
-              // resetCurrentVis method calculates the data and calls methods to
-              // draw the arms
-              $scope.resetCurrentVis = function() {
-                  var currentData = dataByCh[$scope.currentChromosome]
+            // resetCurrentVis method calculates the data and calls methods to
+            // draw the arms
+            $scope.resetCurrentVis = function() {
+                var currentData = dataByCh[$scope.currentChromosome]
 
-                  // reset arm arrays to calculate min/max for current chromosome
-                  vis.q = [];
-                  vis.p = [];
+                // reset arm arrays to calculate min/max for current chromosome
+                vis.q = [];
+                vis.p = [];
 
-                  // separate chromosome data by arm
-                  currentData.forEach(function(d) {
-                      vis[d.arm].push(d);
-                  });
+                // separate chromosome data by arm
+                currentData.forEach(function(d) {
+                    vis[d.arm].push(d);
+                });
 
-                  qmin = d3.min(vis.q, function(d) { return d.genomic_coordinates.start });
-                  qmax = d3.max(vis.q, function(d) { return d.genomic_coordinates.stop });
-                  pmin = d3.min(vis.p, function(d) { return d.genomic_coordinates.start });
-                  pmax = d3.max(vis.p, function(d) { return d.genomic_coordinates.stop });
+                qmin = d3.min(vis.q, function(d) { return d.genomic_coordinates.start });
+                qmax = d3.max(vis.q, function(d) { return d.genomic_coordinates.stop });
+                pmin = d3.min(vis.p, function(d) { return d.genomic_coordinates.start });
+                pmax = d3.max(vis.p, function(d) { return d.genomic_coordinates.stop });
 
-                  // set up x axis, rangeRound used to avoid anti-aliasing issues
-                  x = d3.scale.linear()
+                // set up x axis, rangeRound used to avoid anti-aliasing issues
+                x = d3.scale.linear()
                     .rangeRound([0, document.getElementById('vis').offsetWidth]);
-                  x.domain([0, qmax]);
+                x.domain([0, qmax]);
 
-                  // set values for drawing background and outline arm rects
-                  pArm = [{start: 0, end: (pmax + (qmin-pmax)/2) }]
-                  qArm = [{start: (pmax + (qmin-pmax)/2), end: qmax }]
+                // set values for drawing background and outline arm rects
+                pArm = [{start: 0, end: (pmax + (qmin-pmax)/2) }]
+                qArm = [{start: (pmax + (qmin-pmax)/2), end: qmax }]
 
-                  minDensity = d3.min(currentData, function(d) { return d.density });
-                  maxDensity = d3.max(currentData, function(d) { return d.density });
+                minDensity = d3.min(currentData, function(d) { return d.density });
+                maxDensity = d3.max(currentData, function(d) { return d.density });
 
-                  rainbow.setNumberRange(minDensity, maxDensity);
+                rainbow.setNumberRange(minDensity, maxDensity);
 
-                  // makes the svg responsive
-                  svg.attr("width", document.getElementById('vis').offsetWidth)
+                // makes the svg responsive
+                svg.attr("width", document.getElementById('vis').offsetWidth)
 
-                  // remove all old elements and redraw
-                  svg.selectAll("*").remove();
-                  $scope.drawArm('p', pArm, vis.p);
-                  $scope.drawArm('q', qArm, vis.q);
-              };
+                // remove all old elements and redraw
+                svg.selectAll("*").remove();
+                $scope.drawArm('p', pArm, vis.p);
+                $scope.drawArm('q', qArm, vis.q);
+            };
 
-              // drawArm method uses d3 to do the actual drawing
-              $scope.drawArm = function(armID, armData, armBandData) {
-                  var arm = svg.selectAll("."+armID+"Arm")
-                      .data(armData)
-                  .enter().append("g")
-                      .attr("class", armID+"Arm");
+            // drawArm method uses d3 to do the actual drawing
+            $scope.drawArm = function(armID, armData, armBandData) {
+                var arm = svg.selectAll("."+armID+"Arm")
+                    .data(armData)
+                .enter().append("g")
+                    .attr("class", armID+"Arm");
 
-                  // draws the background lightest gray arm rect
-                  arm.append("rect")
-                      .attr("rx", 5)
-                      .attr("ry", 5)
-                      .attr("width", function(d) { return x(d.end-d.start); })
-                      .attr("height", 50)
-                      .attr("transform", function(d) { return "translate(" + x(d.start) + ",0)" })
-                      .attr("fill", "#fafafa");
+                // draws the background lightest gray arm rect
+                arm.append("rect")
+                    .attr("rx", 5)
+                    .attr("ry", 5)
+                    .attr("width", function(d) { return x(d.end-d.start); })
+                    .attr("height", 50)
+                    .attr("transform", function(d) { return "translate(" + x(d.start) + ",0)" })
+                    .attr("fill", "#fafafa");
 
-                  var armBands = arm.selectAll(".armBand")
-                      .data(armBandData);
+                var armBands = arm.selectAll(".armBand")
+                    .data(armBandData);
 
-                  // draws each band
-                  armBands.enter().append("rect")
-                      .attr("class", "armBand")
-                      .attr("width", function(d) { return x(d.genomic_coordinates.stop-d.genomic_coordinates.start); })
-                      .attr("height", 48)
-                      .attr("transform", function(d) { return "translate(" + x(d.genomic_coordinates.start) + ",1)" })
-                      .style("fill", function(d) { return "#"+rainbow.colorAt(d.density); })
-            	      .on("mouseover", function(d) {
+                // draws each band
+                armBands.enter().append("rect")
+                    .attr("class", "armBand")
+                    .attr("width", function(d) { return x(d.genomic_coordinates.stop-d.genomic_coordinates.start); })
+                    .attr("height", 48)
+                    .attr("transform", function(d) { return "translate(" + x(d.genomic_coordinates.start) + ",1)" })
+                    .style("fill", function(d) { return "#"+rainbow.colorAt(d.density); })
+            	        .on("mouseover", function(d) {
                           var leftOffset = svg[0][0].offsetLeft
                           var topOffset = svg[0][0].offsetTop
                           if(isFirefox) // firefox has an issue with the above declarations so this is a workaround
@@ -142,58 +142,59 @@ app.directive('id3ogram', ['$http', '$window', function($http,$window) {
             	          div .html(d.band_label+"<br/>|")
             	              .style("left", (leftOffset + x(d.genomic_coordinates.start+(d.genomic_coordinates.stop-d.genomic_coordinates.start)/2)-2) + "px")
             	              .style("top", (topOffset-30) + "px");
-            	      })
-            	      .on("mouseout", function(d) {
-                          this.style["fill"] = "#" + rainbow.colorAt(d.density);
-            	          div.transition()
-            	              .duration(500)
-                              .style("opacity", 0);
-            	      });
+            	        })
+            	        .on("mouseout", function(d) {
+                            this.style["fill"] = "#" + rainbow.colorAt(d.density);
+            	            div.transition()
+            	                .duration(500)
+                                .style("opacity", 0);
+            	        });
 
-                  // draws the outline rect
-                  arm.append("rect")
-                      .attr("rx", 5)
-                      .attr("ry", 5)
-                      .attr("width", function(d) { return x(d.end-d.start); })
-                      .attr("height", 50)
-                      .attr("transform", function(d) { return "translate(" + x(d.start) + ",0)" })
-                      .attr("stroke-width", 1)
-                      .attr("stroke", "#111111")
-                      .attr("fill", "none");
-              }
+                // draws the outline rect
+                arm.append("rect")
+                    .attr("rx", 5)
+                    .attr("ry", 5)
+                    .attr("width", function(d) { return x(d.end-d.start); })
+                    .attr("height", 50)
+                    .attr("transform", function(d) { return "translate(" + x(d.start) + ",0)" })
+                    .attr("stroke-width", 1)
+                    .attr("stroke", "#111111")
+                    .attr("fill", "none");
+            }
 
-              // update the vis on window resize for responsiveness
-              w.bind('resize', function () {
-                  $scope.resetCurrentVis();
-              });
+            // update the vis on window resize for responsiveness
+            w.bind('resize', function () {
+                $scope.resetCurrentVis();
+            });
 
-              // load the data and init
-              $http.get(attrs.dataurl).
-          	  success(function(data, status, headers, config) {
-          	    // this callback will be called asynchronously
-          	    // when the response is available
-          		  $scope.info += 'dataset:' + data.dataset;
-          		  $scope.info += '  ID:' + data.dataset_id;
-          		  $scope.info += '  Build:' + data.genome_build;
+            // load the data and init
+            $http.get(attrs.dataurl).
+            success(function(data, status, headers, config) {
+                // this callback will be called asynchronously
+                // when the response is available
+            	$scope.info += 'dataset:' + data.dataset;
+            	$scope.info += '  ID:' + data.dataset_id;
+            	$scope.info += '  Build:' + data.genome_build;
 
-                    data = data.results
+                data = data.results
 
-                    // separate data by chromosomes
-            		  data.forEach(function(d){
-            			  var ch = parseInt(d.genomic_coordinates.chromosome);
+                // separate data by chromosomes
+            	data.forEach(function(d){
+                    var ch = parseInt(d.genomic_coordinates.chromosome);
 
-                          if(!(ch in dataByCh))
-                              dataByCh[ch] = [];
+                     if(!(ch in dataByCh))
+                         dataByCh[ch] = [];
 
-                          dataByCh[ch].push(d);
-                    });
-                    $scope.init();
-          	  }).
-          	  error(function(data, status, headers, config) {
-          	    // called asynchronously if an error occurs
-          	    // or server returns response with an error status.
-          		  $scope.info = 'Failed to load data from SolveBio';
-          	  });
+                     dataByCh[ch].push(d);
+                });
+
+                $scope.init();
+            }).
+            error(function(data, status, headers, config) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+                $scope.info = 'Failed to load data from SolveBio';
+            });
         }
     };
 }]);
